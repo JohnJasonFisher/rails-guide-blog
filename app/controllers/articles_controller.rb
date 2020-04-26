694d23371830a81f9dacfa44886c344537562745
John Fisher <jjfisher91@gmail.com>
2020-04-26 10:49:06 -0500

    use strong params to validate a new Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,13 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(
-            title: params[:title],
-            text: params[:text]
-        )
+        @article = Article.new(article_params)
         if @article.save()
             redirect_to @article
         end
     end
 
-    
+    private def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
