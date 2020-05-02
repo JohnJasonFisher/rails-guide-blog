63299498b95190000934eff93b4608f35f177012
John Fisher <jjfisher91@gmail.com>
2020-05-02 17:07:15 -0500

    add related comments seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
+Comment.delete_all
 Article.delete_all
-Article.create(
+new_articles = Article.create(
     [
         {title: 'Day 1', text: 'When the cats attack.'},
         {title: 'Day 2', text: 'When the dogs attack.'},
         {title: 'Day 3', text: 'When the hamsters attack.'}
     ]
 )
+
+new_articles.each{ |new_article|
+    new_article.comments.create({commenter: 'simpleton', body: 'This is a silly comment!'})
+
+}
